a5951a2f6958b106d7fce4f9a2579756bde81f1d
John1571 <johnny96jones@gmail.com>
2023-02-20 20:42:36 -0600

    Move module 1 exercises to js1 folder

---
+++ js1/ex1.js
@@ -0,0 +1,6 @@
+console.log("Hello world!")
+console.log("This is called 'standard output'.")
+console.log("This stuff in the ( ) is a string.")
+console.log("Start a string with a double-quote.")
+console.log("Write what you want.")
+console.log("Then end with a double-quote.")
---
+++ js1/ex2.js
@@ -0,0 +1,16 @@
+// This is a comment, everything after is ignored.
+
+console.log("I am code");
+
+/* this is also a comment.
+* You use this for longer
+* things that you want to 
+* say */
+
+console.log("so am I");
+
+// The code inside the next comment will not run:
+
+/*
+console.log("I will not run.");
+*/
